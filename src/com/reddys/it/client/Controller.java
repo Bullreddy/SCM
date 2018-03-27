f57640c51e4a5aedd873b84224697ea1ee43ab4f
devareddy <devareddy@DESKTOP-BOII92E>
2018-03-27 23:41:26 +0530

    added commonvo dependecy

--- src/com/reddys/it/client/Controller.java
+++ src/com/reddys/it/client/Controller.java
@@ -1,10 +1,12 @@
 package com.reddys.it.client;
 
 import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.reddys.it.client.ui.LayoutView;
 
 public class Controller {
 	
-	public void execute(RootLayoutPanel panel){
-		
+	public void execute(RootLayoutPanel rootLayoutPanel){
+		LayoutView homeView = new LayoutView();
+		rootLayoutPanel.add(homeView.getGUI());
 	}
 }
